7fb0c5fd85afb925ae20ff5f5734245db27a84a8
Alex Bazhanau <bazhanau@outlook.com>
2019-03-27 14:03:30 +0300

    closeShortPosition v1 impl

--- src/Leverager.sol
+++ src/Leverager.sol
@@ -11,9 +11,9 @@ contract Leverager is DSMath {
     struct Position {
         bytes32 agreementId;
         address owner;
-        address heldToken;
+        IERC20 heldToken;
         uint heldAmount;
-        address principalToken;
+        IERC20 principalToken;
     }
 
     uint public positionsCount;
@@ -34,7 +34,8 @@ contract Leverager is DSMath {
     event Iteration(uint num, uint recievedEthAmount);
 
     event PositionClosed(
-        address indexed owner
+        address indexed owner,
+        uint positionId
     );
 
     // deposit token is a held token
@@ -97,50 +98,98 @@ contract Leverager is DSMath {
         positions[positionId] = Position({
             agreementId: agreementId,
             owner: msg.sender,
-            heldToken: addresses[3],
+            heldToken: IERC20(addresses[3]),
             heldAmount: heldAmount,
-            principalToken: addresses[4]
+            principalToken: IERC20(addresses[4])
         });
 
         emit PositionOpened(msg.sender, positionId, addresses[3], heldAmount, addresses[4], uints[1]);
     }
     
     // FOR DELEGATECALL ONLY!
-    // function closeShortPosition(
-    //     bytes32 positionId,
-    //     address exchange,
-    //     address lender,
-    //     uint maxIterations
-    // ) external {
-    //     Position storage position = positions[positionId];
+    // addresses[0] = lender
+    // addresses[1] = exchange
+    // addresses[2] = priceFeed
+    // uints[0] =     positionId
+    // uints[1] =     wadMaxBaseRatio
+    // uints[2] =     maxIterations
+    function closeShortPosition(
+        address[3] calldata addresses,
+        uint[3] calldata uints
+    ) external {
 
-    //     for (var i = 0; i < maxIterations; i++) {
-    //         uint owedAmount = lender.getOwedAmount(position.agreementId, position.principalToken);
+        /*
+        - get needed held token amount for swap
+        - swap back to principal
+        - repay principal
+        - return collateral
+        */
+        Position storage position = positions[uints[0]];
 
-    //         bool ok;
-    //         bytes memory result;
-    //         (ok, result) = exchange.delegatecall(
-    //             abi.encodeWithSignature(
-    //                 "swap(address,uint256,address,uint256)",
-    //                 position.heldToken, 0, position.principalToken, owedAmount
-    //             )
-    //         );
-    //         require(ok, "swap failed");
-    //         recievedAmount = uint(_bytesToBytes32(result));
+        for (uint i = 0; i < uints[2]; i++) {
+            uint owedAmountInPrincipalToken = ILender(addresses[0]).getOwedAmount(position.agreementId, position.principalToken);
 
-    //         (ok, result) = lender.delegatecall(
-    //             abi.encodeWithSignature(
-    //                 "repayAndReturn(bytes32,address,uint256,address,uint256)",
-    //                 position.agreementId, position.principalToken, uint(-1), position.heldToken, uint(-1)
-    //             )
-    //         );
-    //         require(ok, "supplyAndBorrow failed");
-    //     }
+            // TODO: EXCHANGE price feed
+            uint owedAmountInHeldToken = IPriceFeed(addresses[2]).convertAmount(position.principalToken, owedAmountInPrincipalToken, position.heldToken);
+            uint heldTokenBalance = position.heldToken.balanceOf(address(this));
 
-    //     delete positions[positionId];
+            // do we have enough tokens to repay all debt?
+            if (owedAmountInHeldToken > heldTokenBalance) {
+                bool ok;
+                bytes memory result;
+                (ok, result) = addresses[1].delegatecall(
+                    abi.encodeWithSignature(
+                        "swap(address,uint256,address,uint256)",
+                        position.heldToken, heldTokenBalance, position.principalToken, 0
+                    )
+                );
+                require(ok, "swap failed");
+                uint recievedAmount = uint(_bytesToBytes32(result));
+
+                // TODO: LENDER price feed
+                uint withdrawalAmount = calcFreeCollateral(
+                    position,
+                    owedAmountInPrincipalToken,
+                    recievedAmount,
+                    uints[1],
+                    IPriceFeed(addresses[2])
+                );
+
+                (ok, result) = addresses[0].delegatecall(
+                    abi.encodeWithSignature(
+                        "repayAndReturn(bytes32,address,uint256,address,uint256)",
+                        position.agreementId, position.principalToken, recievedAmount, position.heldToken, withdrawalAmount
+                    )
+                );
+                require(ok, "supplyAndBorrow failed");
+            } else {
+                bool ok;
+                bytes memory result;
+                (ok, result) = addresses[1].delegatecall(
+                    abi.encodeWithSignature(
+                        "swap(address,uint256,address,uint256)",
+                        position.heldToken, 0, position.principalToken, owedAmountInPrincipalToken
+                    )
+                );
+                require(ok, "swap failed");
+                uint recievedAmount = uint(_bytesToBytes32(result));
+
+                (ok, result) = addresses[0].delegatecall(
+                    abi.encodeWithSignature(
+                        "repayAndReturn(bytes32,address,uint256,address,uint256)",
+                        position.agreementId, position.principalToken, uint(-1), position.heldToken, uint(-1)
+                    )
+                );
+                require(ok, "supplyAndBorrow failed");
+
+                break;
+            }
+        }
+
+        delete positions[uints[0]];
         
-    //     emit PositionClosed(msg.sender);
-    // }
+        emit PositionClosed(msg.sender, uints[0]);
+    }
 
     // determines how much we can borrow from a lender in order to maintain provided collateral ratio
     function calcPrincipal(
@@ -153,6 +202,24 @@ contract Leverager is DSMath {
         uint collateralETH = priceFeed.convertAmountToETH(heldToken, depositAmount);
         uint principalETH = wdiv(collateralETH, wadMaxBaseRatio);
         principalAmount = priceFeed.convertAmountFromETH(principalToken, principalETH);
+    }
+
+    // TODO: should we move it to the Lender?
+    function calcFreeCollateral(
+        Position storage position,
+        uint currentDebt,
+        uint repaymentAmountInPrincipalToken,
+        uint wadMaxBaseRatio,
+        IPriceFeed priceFeed
+    ) internal view returns (uint freeCollateralAmount){
+        // freeCollateral = heldCollateral - neededCollateral
+        // neededCollateral = remainingPrincipal * collRatio
+        // remainingPrincipal = currentPrincipal - repaymentAmount
+        uint remainingDebt = sub(currentDebt, repaymentAmountInPrincipalToken);
+        uint remainingDebtETH = priceFeed.convertAmountToETH(position.principalToken, remainingDebt);
+        uint neededCollateralETH = wmul(remainingDebtETH, wadMaxBaseRatio);
+        uint neededCollateral = priceFeed.convertAmountFromETH(position.heldToken, neededCollateralETH);
+        freeCollateralAmount = sub(position.heldAmount, neededCollateral);
     }
 
     function _bytesToBytes32(bytes memory source) internal pure returns (bytes32 result) {
